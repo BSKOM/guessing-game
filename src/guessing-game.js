9827ccb4db121b16ebb71e2eb5ed6ab022928264
BSKOM <b.shelestua@gmail.com>
2021-08-02 23:00:41 +0300

    feat: add guesing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
 
-    setRange(min, max) {
+  constructor() {
+    this.min = 0
+    this.max = 0
+    this.g = 0
+  }
+  setRange(a, b) {
+    this.min = a
+    this.max = b
+  }
+  guess() {
+    this.g = Math.ceil((this.min + this.max) / 2)
+    return this.g
+  }
 
-    }
+  lower() {
+    this.setRange(this.min, this.g--)
+  }
 
-    guess() {
+  greater() {
+    this.setRange(this.g++, this.max)
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
 }
 
 module.exports = GuessingGame;
